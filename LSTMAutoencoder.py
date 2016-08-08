c13987753ab6382ffe6bd025ef760ef19e710c65
iwyoo <doveu1234@gmail.com>
2016-08-08 13:48:00 +0900

    seq. latent codes

--- LSTMAutoencoder.py
+++ LSTMAutoencoder.py
@@ -4,6 +4,10 @@ from tensorflow.python.ops.rnn_cell import BasicLSTMCell
 
 import numpy as np
 
+"""
+Future : Modularization
+"""
+
 class BasicLSTMAutoencoder(object):
   def __init__(self, hidden_num, inputs, 
     cell=None, optimizer=None, reverse=True, 
@@ -51,6 +55,75 @@ class BasicLSTMAutoencoder(object):
     with tf.variable_scope('decoder'):
       dec_output, dec_state = tf.nn.rnn(
         self._dec_cell, dec_inputs, 
+        initial_state=enc_state, dtype=tf.float32)
+      dec_weight_ = tf.Variable(
+        tf.truncated_normal([hidden_num, self.elem_num], dtype=tf.float32),
+        name="dec_weight")
+      dec_bias_ = tf.Variable(
+        tf.constant(0.1, shape=[self.elem_num], dtype=tf.float32),
+        name="dec_bias")
+
+    if reverse:
+      dec_output = dec_output[::-1]
+
+    """the shape of each tensor
+      dec_output_ : (step_num x hidden_num)
+      dec_weight_ : (hidden_num x elem_num)
+      dec_bias_ : (elem_num)
+      output_ : (step_num x elem_num)
+      input_ : (step_num x elem_num)
+    """
+    dec_output_ = tf.transpose(tf.pack(dec_output), [1,0,2])
+    dec_weight_ = tf.tile(tf.expand_dims(dec_weight_, 0), [self.batch_num,1,1])
+
+    self.output_ = tf.batch_matmul(dec_output_, dec_weight_) + dec_bias_
+    self.input_ = tf.transpose(tf.pack(inputs), [1,0,2])
+    self.loss = tf.reduce_mean(tf.square(self.input_ - self.output_))
+
+    if optimizer is None :
+      self.train = tf.train.AdamOptimizer().minimize(self.loss)
+    else :
+      self.train = optimizer.minimise(self.loss)
+
+class SequentialLSTMAutoencoder(object):
+  def __init__(self, hidden_num, inputs, 
+    cell=None, optimizer=None, reverse=True):
+    """Sequence version of LSTM-autoencoder.
+    The decoder takes hidden codes of encoder cells.
+
+    Args:
+      hidden_num : number of hidden elements of each LSTM unit.
+      inputs : a list of input tensors  with size 
+              (batch_num x elem_num)
+      cell : an rnn cell object (the default option 
+            is `tf.python.ops.rnn_cell.BasicLSTMCell`)
+      optimizer : optimizer for rnn (the default option is
+              `tf.train.AdamOptimizer`)
+      reverse : Option to decode in reverse order.
+    """
+
+    self.batch_num = inputs[0].get_shape().as_list()[0]
+    self.elem_num = inputs[0].get_shape().as_list()[1]
+
+    if cell is None:
+      self._enc_cell = BasicLSTMCell(hidden_num)
+      self._dec_cell = BasicLSTMCell(hidden_num)
+    else :
+      self._enc_cell = cell(hidden_num)
+      self._dec_cell = cell(hidden_num)
+
+    with tf.variable_scope('encoder'):
+      self.z_codes, enc_state = tf.nn.rnn(
+        self._enc_cell, inputs, dtype=tf.float32)
+
+    if reverse:
+      dec_inputs = self.z_codes[::-1]
+    else :
+      dec_inputs = self.z_codes
+
+    with tf.variable_scope('decoder'):
+      dec_output, dec_state = tf.nn.rnn(
+        self._dec_cell, dec_inputs,
         initial_state=enc_state, dtype=tf.float32)
       dec_weight_ = tf.Variable(
         tf.truncated_normal([hidden_num, self.elem_num], dtype=tf.float32),
